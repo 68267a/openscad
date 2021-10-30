c26644c391608a019d4ae6716ec9cc8893155bf3
accountnumber3 <no>
2021-10-30 10:19:43 -0400

    utoijg

--- lenticularPython/lentigram.py
+++ lenticularPython/lentigram.py
@@ -1,20 +1,28 @@
-
-# Importing Image class from PIL module
+import os
 from PIL import Image
-# Opens a image in RGB mode
-im = Image.open(r"pikachu.png")
-# Size of the image in pixels (size of original image)
-# (This is not mandatory)
-width, height = im.size
-# Setting the points for cropped image
-left = 6
-top = height / 4
-right = 174
-bottom = 3 * height / 4
-# Cropped image of above dimension
-# (It will not change original image)
-im1 = im.crop((left, top, right, bottom))
-newsize = (200, 200)
-im1 = im1.resize(newsize)
-# Shows the image in image viewer
-im1.show()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+strips = 8
+
+# im1 = Image.open(r"batman.png")
+# im2 = Image.open(r"darthvader.png")
+
+ima = Image.open(r"colors.png")
+width, height = ima.size
+left = 0
+top = 0
+right = width/strips
+bottom = height
+
+ima0 = ima.crop((left,top,right,bottom))
+ima1 = ima0.transpose(method=Image.FLIP_LEFT_RIGHT)
+print(right)
+
+# ima.show()
+# ima0.show()
+# im_flipped.show()
+
+imgOut = Image.new("RGB", (width*2, height), "black")
+imgOut.paste(ima0, (0,0))
+imgOut.paste(ima1, (44,0))
+imgOut.show()
