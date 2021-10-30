5bae6775e0b0122692287728d4fd9a12e4fa5aa7
accountnumber3 <no>
2021-10-30 13:32:46 -0400

    sf

--- lenticularPython/lentigram.py
+++ lenticularPython/lentigram.py
@@ -1,28 +1,31 @@
 import os
 from PIL import Image
 
+numstrips = 8
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-strips = 8
-
-# im1 = Image.open(r"batman.png")
-# im2 = Image.open(r"darthvader.png")
-
 ima = Image.open(r"colors.png")
+imb = ima.transpose(method=Image.FLIP_LEFT_RIGHT)
 width, height = ima.size
 left = 0
 top = 0
-right = width/strips
+right = round(width/numstrips)
 bottom = height
 
+strip = 0
+
+while strip < numstrips:
+	print("strip")
+
+
+	strip += 1
+
+
+
+
 ima0 = ima.crop((left,top,right,bottom))
-ima1 = ima0.transpose(method=Image.FLIP_LEFT_RIGHT)
-print(right)
 
-# ima.show()
-# ima0.show()
-# im_flipped.show()
 
-imgOut = Image.new("RGB", (width*2, height), "black")
-imgOut.paste(ima0, (0,0))
-imgOut.paste(ima1, (44,0))
-imgOut.show()
+# imgOut = Image.new("RGB", (width*2, height), "black")
+# imgOut.paste(ima0, (0,0))
+# imgOut.paste(ima1, (right,0))
+# imgOut.show()
