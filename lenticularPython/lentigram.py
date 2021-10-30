a9646a1b6e0b75a2fce464112a3571853c8e2e1d
accountnumber3 <33899447+accountnumber3@users.noreply.github.com>
2021-10-30 15:02:07 -0400

    err?

--- lenticularPython/lentigram.py
+++ lenticularPython/lentigram.py
@@ -1,31 +1,30 @@
 import os
 from PIL import Image
 
-numstrips = 8
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 ima = Image.open(r"colors.png")
 imb = ima.transpose(method=Image.FLIP_LEFT_RIGHT)
+imb.save('colorsflipped.png')
+numstrips = 8
 width, height = ima.size
-left = 0
-top = 0
-right = round(width/numstrips)
-bottom = height
+stripwidth = round(width/numstrips)
 
+print("width" + str(width)+ " stripwidth" + str(stripwidth))
 strip = 0
+h = height
+x = 0
+y = stripwidth
 
+imgOut = Image.new("RGB", (width*2, height), "black")
 while strip < numstrips:
-	print("strip")
-
-
+	print("strip" + str(strip) + " x" + str(x) + " y" + str(y))
 	strip += 1
+	strip_a = ima.crop((x,0,y,h))
+	strip_b = imb.crop((x,0,y,h))
+	imgOut.paste(strip_a,(x,0))
+	imgOut.paste(strip_b,(x+stripwidth,0))
+	x=x+stripwidth
+	y=y+stripwidth
 
 
-
-
-ima0 = ima.crop((left,top,right,bottom))
-
-
-# imgOut = Image.new("RGB", (width*2, height), "black")
-# imgOut.paste(ima0, (0,0))
-# imgOut.paste(ima1, (right,0))
-# imgOut.show()
+imgOut.save('out.png')
